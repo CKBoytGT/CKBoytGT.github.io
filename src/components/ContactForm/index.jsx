a7290a25e772caf6af4abdd2e6c947f7bf67bef7
C K Boyt <ckboyt@gmail.com>
2023-11-06 12:38:38 -0500

    Install and implement emailjs

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,26 +1,58 @@
 import "./index.css";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
+  const [sendStatus, setSendStatus] = useState("");
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    setSendStatus("Sending");
+
+    emailjs
+      .sendForm(
+        "service_gbhel8j",
+        "template_6h0cmsc",
+        form.current,
+        "boMIdbpmZB6qujKMl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSendStatus("Message sent.");
+        },
+        (error) => {
+          console.log(error.text);
+          setSendStatus("Error! Please email me@ckboyt.com directly.");
+        }
+      );
+  };
+
   return (
     <div id="contact-form">
-      <form action="mailto:me@ckboyt.com" method="POST" encType="text/plain">
+      <form ref={form} onSubmit={sendEmail}>
         <label className="form-row">
           Name:
-          <input type="text" name="Name" autoComplete="on"></input>
+          <input type="text" name="user_name"></input>
         </label>
         <label className="form-row">
           Email:
-          <input
-            type="email"
-            name="Preferred Contact Email"
-            autoComplete="on"
-          ></input>
+          <input type="email" name="user_email"></input>
         </label>
         <label className="form-textarea">
           Message:
-          <textarea name="Message" rows="8"></textarea>
+          <textarea name="message" rows="8"></textarea>
         </label>
-        <button>Send</button>
+        <div id="send-bar">
+          <p>
+            {sendStatus}
+            {sendStatus === "Sending" ? <span id="dots">&#x2026;</span> : ""}
+          </p>
+          <button type="submit">Send</button>
+        </div>
       </form>
     </div>
   );
